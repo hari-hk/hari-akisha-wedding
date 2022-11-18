7b60847845385d3a07ebd7ea45dceabab225ccb0
hari hara kumar <hariharakumar5196@gmail.com>
2022-11-18 23:56:14 +0530

    change UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'hariaki';
+  // 8.312607, 77.210299
+  openInMap() {
+    window.open('https://goo.gl/maps/NJwKxuiGvmahandR9', '_blank');
+  }
 }
