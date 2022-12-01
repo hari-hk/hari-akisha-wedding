160a120d0dc11863acac0d7104528dc083820e9f
hari-hk <hariharakumar5196@gmail.com>
2022-12-01 23:41:31 +0530

    add music

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { Howl, Howler } from 'howler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'hariaki';
   // 8.312607, 77.210299
+
+  sound = new Howl({
+    src: ['../assets/Anbil-Avan.mp3'],
+    autoplay: true,
+    loop: true,
+    volume: 0.2,
+    onend: function () {
+      console.log('Finished!');
+    },
+  });
+
   openInMap() {
     window.open('https://goo.gl/maps/NJwKxuiGvmahandR9', '_blank');
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.sound.play();
+    }, 300);
   }
 }
