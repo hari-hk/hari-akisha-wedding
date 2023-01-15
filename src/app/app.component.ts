82d76799ac13d87eaa2d2aedc0133723a4f6a672
hari-hk <hariharakumar5196@gmail.com>
2023-01-15 20:57:55 +0530

    update address

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { Howl, Howler } from 'howler';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,8 @@ import { Howl, Howler } from 'howler';
 export class AppComponent implements AfterViewInit {
   title = 'hariaki';
   sound = new Howl({
-    src: ['../assets/Anbil-Avan.mp3'],
-    autoplay: false,
+    src: ['../assets/Naan_Pizhai.mp3'],
+    autoplay: environment.production,
     loop: true,
     volume: 0.1,
     onend: function () {
@@ -20,15 +21,16 @@ export class AppComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     setTimeout(() => {
-      // this.sound.play();
+      this.sound.play();
     }, 300);
   }
 
   openInMap(type: string) {
     const location =
       type === 'wedding'
-        ? 'https://www.google.com/maps/dir//8666%2BCH8+YMCA+Marthandam+Conference+Hall,+Marthandam,+Tamil+Nadu+629165/@8.3110342,77.2114621,17z/data=!4m16!1m6!3m5!1s0x3b0455248365316d:0xcb5c190c0072302c!2sYMCA+Marthandam+Conference+Hall!8m2!3d8.3110325!4d77.2114608!4m8!1m0!1m5!1m1!1s0x3b0455248365316d:0xcb5c190c0072302c!2m2!1d77.2114608!2d8.3110325!3e2'
-        : 'https://www.google.com/maps/dir//75VQ%2B9JF,+Kunnathoor,+Tamil+Nadu+629162/@8.2934319,77.1190006,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3b05ab389477598b:0x3e29c528a4d37f6a!2m2!1d77.1890412!2d8.293438';
+        ? 'https://goo.gl/maps/hpZFBNdh7Yxcp1ti6'
+        : 'https://goo.gl/maps/6fvSQLGkTD2evziN7';
+
     window.open(location, '_blank');
   }
 }
